1971acc06cf3c3debc4fde9f16aaa20a892e4fea
USLHihnW <ladon95@gmail.com>
2021-05-29 15:40:06 +0300

    Migration mechanism has been implemented instead of manual DB commits from the terminal. The first migration has been generated and applied.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,15 +3,25 @@ from datetime import date
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 """the application factory function"""
 def create_app(test_config=None):
+    from flaskr.models import Car
+    from flaskr.models import Contract
+    from flaskr.models import User
+    from flaskr.models import Brand
+    from flaskr.models import Location
+    from flaskr.models import CarType
+
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config.ProdConfig')
+    app.config.from_object('config.DevConfig')
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # ensure the instance folder exists
     try:
