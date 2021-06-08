d504eedf28949ffca840307744b2d938a9defd93
USLHihnW <ladon95@gmail.com>
2021-06-08 13:22:05 +0300

    Phone number field has been fixed.

--- flaskr/models/user.py
+++ flaskr/models/user.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    phone_number = db.Column(db.String(200), unique=True, nullable=False)
+    phone_number = db.Column(db.String(15), unique=True, nullable=False)
 
     hashed_password = db.Column(db.String(200), unique=False, nullable=False)
 
